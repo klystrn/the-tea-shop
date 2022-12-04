0b8eb6c04590e465101410a9da1aed38485f1e14
Reginald Tan <66867651+klystrn@users.noreply.github.com>
2022-12-04 12:42:56 +0800

    Commit 4.3
    - Finished the welcome section on home page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,14 +1,12 @@
 // ==================== FOOTER ICON BUTTONS ====================
 function footerButtons(){
-    function footerButtonFacebook(){ window.open("https://www.facebook.com/", '_blank'); }
+    function footerButtonFacebook(){ window.open("https://www.facebook.com/", '_blank') }
     function footerButtonTwitter(){ window.open("https://www.twitter.com/", '_blank') }
     function footerButtonInstagram(){ window.open("https://www.instagram.com/", '_blank') }
-    function footerButtonGithub(){ window.open("https://github.com/klystrn/the-tea-shop", '_blank') }
 
     document.querySelector(".footer_button_facebook").onclick = function () { footerButtonFacebook() };
     document.querySelector(".footer_button_twitter").onclick = function () { footerButtonTwitter() };
     document.querySelector(".footer_button_instagram").onclick = function () { footerButtonInstagram() };
-    document.querySelector(".footer_button_github").onclick = function () { footerButtonGithub() };
 }
 
 footerButtons()
